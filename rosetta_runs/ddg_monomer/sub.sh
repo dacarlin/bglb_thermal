4abc87b897e74dc6f32242a7b5031dba231ef684
Alex Carlin <carlin@ucdavis.edu>
2016-06-07 02:12:46 -0700

    Updates

--- rosetta_runs/ddg_monomer/sub.sh
+++ rosetta_runs/ddg_monomer/sub.sh
@@ -3,6 +3,8 @@
 #SBATCH --job-name=ddg
 #SBATCH --output=logs/slurm-%A_%a.out
 #SBATCH --error=logs/slurm-%A_%a.err
+#SBATCH -p gc128 
 
-MUT=$( sed -n "$SLURM_ARRAY_TASK_ID p" list ) 
-/share/work/rosetta/source/bin/ddg_monomer.linuxgccrelease @flags -resfile resfiles/$MUT
+cd out # sigh 
+MUT=$( sed -n "$SLURM_ARRAY_TASK_ID p" ../list ) 
+/share/work/rosetta/source/bin/ddg_monomer.linuxgccrelease @flags -resfile ../res/${MUT}.res
