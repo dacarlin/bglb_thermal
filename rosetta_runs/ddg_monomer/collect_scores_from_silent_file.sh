3ef56e978ffe7933227ff19dc781d3759a57d247
Alex Carlin <carlin@ucdavis.edu>
2016-07-27 00:56:46 -0700

    Lots of updates and changes

--- rosetta_runs/ddg_monomer/collect_scores_from_silent_file.sh
+++
@@ -1,4 +0,0 @@
-for i in out/*out; do 
-  printf "%s " $i
-  head -4 $i | tail -n 1 
-done 
--- rosetta_runs/fast_relax/make_list.py
+++
@@ -1,6 +0,0 @@
-import pandas 
-from Bio.SeqUtils import seq3
-df = pandas.read_csv( 'mutant_list', header=None ) 
-flag = '-suffix _{} -parser:script_vars target={} new_res={}'
-df[0] = df[0].map( lambda x: flag.format( x, x[1:-1], seq3(x[-1]).upper() ) ) 
-df.to_csv( 'list', index=False ) 
